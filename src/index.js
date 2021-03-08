b9a0cf0d1303bdeee5516f818a4bc43d9ab3200f
Запитецкая Алена Евгеньевна <190032@edu.psuti.ru>
2021-03-08 23:50:41 +0400

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) n *= (-1);
+    let val = n.toString().split('').map(Number);
+    let val1 = [];
+    let j = val.length-1;
+    for (let i =0; i < val.length; i++){
+            val1[i] = val[j];
+            j--;
+    }
+    return val1.join("");
 }
